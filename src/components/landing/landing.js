8e305022af19aee1e9e8d1f01e52eb5c792c1f36
lohpaul9 <lohpaul9@seas.upenn.edu>
2023-01-29 11:21:41 -0500

    completed basic

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -1,103 +1,102 @@
 import React from 'react'
-import "../../Web3Builders.css"
 import sponsorsPic from '../../images/62fafc331e697b8d56ead84b_hbc-logos.png'
 import somePic from '../../images/62fb0424f4027807fc5ccd1f_hbc-4.png'
 import Team from './team'
 
 export default function Landing() {
-  return (
-    <>
-      <div className="section-14 wf-section">
-          <div className="wide-div z">
-              <div className="inmost-div">
-                  <div className="div-block-44">
-                      <h1 style={{'opacity': 1}} className="hero-header-new">A
-                          TOP &nbsp;STUDENT-RUN BLOCKCHAIN INNOVATION&nbsp;HUB</h1>
-                      <p style={{'opacity': 1}} className="hero-sub">Build with
-                          some of the best in Web3, do &nbsp;cutting-edge research, and find career opportunities through
-                          our deep industry connections.</p>
-                      <a style={{'opacity': 1}}
-                          href="https://join.slack.com/t/web3builders-group/shared_invite/zt-1ig67kr78-Gq1fhG92HoitvPNw0yvPbw" target="_blank" className="nav-link-2">JOIN &nbsp;US
-                          &gt;</a>
-                  </div>
-              </div>
-          </div>
-      </div>
-      <div style={{'opacity': 1}} className="section-15 wf-section">
-          <div className="wide-div">
-              <div className="div-block-37 pad">
-                  <div className="div-block-38"></div>
-              </div>
-              <div className="inner-div">
-                  <div className="inmost-div">
-                      <img src={sponsorsPic} loading="lazy"
-                          sizes="(max-width: 479px) 100vw, 90vw" alt="" className="image-7"></img>
-                  </div>
-              </div>
-          </div>
-      </div>
-      <div className="section-15 wf-section">
-          <div className="wide-div">
-              <div className="div-block-37">
-                  <div className="div-block-38"></div>
-              </div>
-              <div className="inner-div">
-                  <div className="inmost-div">
-                      <div className="w-row">
-                          <div className="w-col w-col-6">
-                              <div className="col-div">
-                                  <img src={somePic} loading="lazy"
-                                      sizes="(max-width: 479px) 100vw, (max-width: 767px) 87vw, 43vw" alt=""></img>
-                                  <div className="text-block-8">EVENTS</div>
-                                  <p className="paragraph-12">We organize panels, talks, and workshops with some of the
-                                      greatest builders, investors, and researchers in the blockchain space. The capstone
-                                      of the year is our annual HBC conference, open to the broader Philadelphia community.</p>
-                              </div>
-                          </div>
-                          <div className="w-col w-col-6">
-                              <div className="col-div">
-                                  <img src={somePic} loading="lazy"
-                                      sizes="(max-width: 479px) 100vw, (max-width: 767px) 87vw, 43vw" alt=""></img>
-                                  <div className="text-block-8">LABS</div>
-                                  <p className="paragraph-12">Join a community of Upenn's top builders in Web3. We host
-                                      technical workshops, organize blockchain-focused hackathons, build projects for
-                                      leading industry partners, and support Web3 startups in the Penn
-                                      ecosystem.</p>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-              <div className="inner-div">
-                  <div className="inmost-div">
-                      <div className="w-row">
-                          <div className="w-col w-col-6">
-                              <div className="col-div">
-                                  <img src={somePic} loading="lazy"
-                                      sizes="(max-width: 479px) 100vw, (max-width: 767px) 87vw, 43vw" alt=""/>
-                                  <div className="text-block-8">101</div>
-                                  <p className="paragraph-12">A six-week introduction to foundational topics including
-                                      blockchain, smart contracts, DAOs, NFTs, DeFi, and more. Taught in small weekly
-                                      discussion groups, this is the best way to go from “zero-to-one” in your knowledge
-                                      of the space.</p>
-                              </div>
-                          </div>
-                          <div className="w-col w-col-6">
-                              <div className="col-div">
-                                  <img src={somePic} loading="lazy"
-                                      sizes="(max-width: 479px) 100vw, (max-width: 767px) 87vw, 43vw" alt=""/>
-                                  <div className="text-block-8">TALENT</div>
-                                  <p className="paragraph-12">Our members benefit from our extensive hiring network,
-                                      crypto-related research projects with Harvard professors, and more. To date, we’ve
-                                      matched dozens of club members with internship and full-time job opportunities.</p>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-      <Team/>
-    </>
-  )
+    return (
+        <>
+            <div className="section-14 wf-section">
+                <div className="wide-div z">
+                    <div className="inmost-div">
+                        <div className="div-block-44">
+                            <h1 style={{ 'opacity': 1 }} className="hero-header-new">A
+                                TOP &nbsp;STUDENT-RUN BLOCKCHAIN INNOVATION&nbsp;HUB</h1>
+                            <p style={{ 'opacity': 1 }} className="hero-sub">Build with
+                                some of the best in Web3, do &nbsp;cutting-edge research, and find career opportunities through
+                                our deep industry connections.</p>
+                            <a style={{ 'opacity': 1 }}
+                                href="https://join.slack.com/t/web3builders-group/shared_invite/zt-1ig67kr78-Gq1fhG92HoitvPNw0yvPbw" target="_blank" className="nav-link-2">JOIN &nbsp;US
+                                &gt;</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div style={{ 'opacity': 1 }} className="section-15 wf-section">
+                <div className="wide-div">
+                    <div className="div-block-37 pad">
+                        <div className="div-block-38"></div>
+                    </div>
+                    <div className="inner-div">
+                        <div className="inmost-div">
+                            <img src={sponsorsPic} loading="lazy"
+                                sizes="(max-width: 479px) 100vw, 90vw" alt="" className="image-7"></img>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="section-15 wf-section">
+                <div className="wide-div">
+                    <div className="div-block-37">
+                        <div className="div-block-38"></div>
+                    </div>
+                    <div className="inner-div">
+                        <div className="inmost-div">
+                            <div className="w-row">
+                                <div className="w-col w-col-6">
+                                    <div className="col-div">
+                                        <img src={somePic} loading="lazy"
+                                            sizes="(max-width: 479px) 100vw, (max-width: 767px) 87vw, 43vw" alt=""></img>
+                                        <div className="text-block-8">EVENTS</div>
+                                        <p className="paragraph-12">We organize panels, talks, and workshops with some of the
+                                            greatest builders, investors, and researchers in the blockchain space. The capstone
+                                            of the year is our annual HBC conference, open to the broader Philadelphia community.</p>
+                                    </div>
+                                </div>
+                                <div className="w-col w-col-6">
+                                    <div className="col-div">
+                                        <img src={somePic} loading="lazy"
+                                            sizes="(max-width: 479px) 100vw, (max-width: 767px) 87vw, 43vw" alt=""></img>
+                                        <div className="text-block-8">LABS</div>
+                                        <p className="paragraph-12">Join a community of Upenn's top builders in Web3. We host
+                                            technical workshops, organize blockchain-focused hackathons, build projects for
+                                            leading industry partners, and support Web3 startups in the Penn
+                                            ecosystem.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="inner-div">
+                        <div className="inmost-div">
+                            <div className="w-row">
+                                <div className="w-col w-col-6">
+                                    <div className="col-div">
+                                        <img src={somePic} loading="lazy"
+                                            sizes="(max-width: 479px) 100vw, (max-width: 767px) 87vw, 43vw" alt="" />
+                                        <div className="text-block-8">101</div>
+                                        <p className="paragraph-12">A six-week introduction to foundational topics including
+                                            blockchain, smart contracts, DAOs, NFTs, DeFi, and more. Taught in small weekly
+                                            discussion groups, this is the best way to go from “zero-to-one” in your knowledge
+                                            of the space.</p>
+                                    </div>
+                                </div>
+                                <div className="w-col w-col-6">
+                                    <div className="col-div">
+                                        <img src={somePic} loading="lazy"
+                                            sizes="(max-width: 479px) 100vw, (max-width: 767px) 87vw, 43vw" alt="" />
+                                        <div className="text-block-8">TALENT</div>
+                                        <p className="paragraph-12">Our members benefit from our extensive hiring network,
+                                            crypto-related research projects with Harvard professors, and more. To date, we’ve
+                                            matched dozens of club members with internship and full-time job opportunities.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Team />
+        </>
+    )
 }
--- src/components/landing/team.js
+++ src/components/landing/team.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import "../../Web3Builders.css"
 
-function TeamMember({url, name}) {
+
+function TeamMember({ url, name, desc }) {
   return (
     <>
-      <a href={url} target="_blank" className="team-link">
-        <span className="team-names">{name}  </span>
+      <a href={url} target={url} className="m-3 flex flex-col items-center w-32 h-42 p-2 hover:shadow-lg rounded-lg box-border transition-shadow duration-500">
+        <img className='w-24 h-28 object-cover rounded-sm' src="https://thumbs.dreamstime.com/b/golden-retriever-dog-21668976.jpg" ></img>
+        <div className="font-abel text-xl">{name}</div>
+        <div className="font-abel text-m">{desc}</div>
       </a>
     </>
   )
@@ -13,38 +15,30 @@ function TeamMember({url, name}) {
 
 export default function Team() {
   const teamMembers = [
-    {url: "paul.com", name: "paul"},
-    {url: "intel.com", name: "paul"}
+    { url: "https://github.com/lohpaul9", name: "Paul", desc: "Guru" },
+    { url: "paul.com", name: "Paul", desc: "Guru" },
+    { url: "paul.com", name: "Paul", desc: "Guru" },
   ]
   const advisors = [
-    {url: "paul.com", name: "advisor 1"},
-    {url: "intel.com", name: "advisor 2"}
+    { url: "paul.com", name: "Paul", desc: "Guru" },
+    { url: "paul.com", name: "Paul", desc: "Guru" },
   ]
   return (
-    <div className="section-15 wf-section">
-        <div className="wide-div">
-            <div className="div-block-37 pad">
-                <div className="div-block-38"></div>
-            </div>
-            <div className="wide-div">
-                <div className="inner-div">
-                    <div className="inmost-div color">
-                        <p className="paragraph-13">
-                            CORE:
-                            {teamMembers.map(member => {
-                              return (<TeamMember url={member.url} name={member.name}/>)
-                            })}
-                        </p>
-                        <p className="paragraph-13 no-bottom">
-                            ADVISORS:
-                            {advisors.map(member => {
-                              return (<TeamMember url={member.url} name={member.name}/>)
-                            })}
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div className="flex flex-col items-center border-2 border-gray-300 mx-44 my-14 pb-2">
+      <div className='mt-8 text-3xl font-abel'>MEET THE CORE TEAM</div>
+      <div className="flex justify-center flex-wrap">
+        {teamMembers.map(member => {
+          return (<TeamMember url={member.url} name={member.name} desc={member.desc} />)
+        })}
+      </div>
+      <div className='text-3xl font-abel'>ADVISORS</div>
+      <div className="flex justify-center flex-wrap">
+        {advisors.map(member => {
+          return (<TeamMember url={member.url} name={member.name} desc={member.desc} />)
+        })}
+      </div>
+
     </div>
+
   )
 }
